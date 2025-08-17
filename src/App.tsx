22c9659962c216f0a0e977b5bc5d94898d41c7a9
Kwon DaeGeun <dk31415@naver.com>
2025-08-18 08:40:55 +0900

    style: biome lint 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Button } from "./components/ui/button";
 import { Camera } from "lucide-react";
+import { Button } from "./components/ui/button";
 
 function App() {
-	return (
-		<div>
-			TailwindCSS 적용
-			<Button type="button">버튼</Button>
-			<Camera />
-		</div>
-	);
+    return (
+        <div>
+            TailwindCSS 적용
+            <Button type="button">버튼</Button>
+            <Camera />
+        </div>
+    );
 }
 
 export default App;
