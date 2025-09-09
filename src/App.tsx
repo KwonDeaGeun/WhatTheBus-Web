e7175bfc9c8311131c85d896cb3dbf273fae923b
Kwon DaeGeun <dk31415@naver.com>
2025-09-09 19:54:03 +0900

    feat: 단국대 정문 및 상경관 앞 정류장 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,188 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 declare global {
-  interface Window {
-    kakao: any;
-  }
+    interface Window {
+        kakao: any;
+    }
 }
 
 function App() {
-  useEffect(() => {
-    const kakaoApiKey = import.meta.env.VITE_KAKAO_MAP_API_KEY;
+    useEffect(() => {
+        const kakaoApiKey = import.meta.env.VITE_KAKAO_MAP_API_KEY;
 
-    const loadKakaoMapScript = () => {
-      if (window.kakao && window.kakao.maps) {
-        initMap();
-        return;
-      }
+        const loadKakaoMapScript = () => {
+            if (window.kakao && window.kakao.maps) {
+                initMap();
+                return;
+            }
 
-      const script = document.createElement('script');
-      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoApiKey}&autoload=false`;
-      script.async = true;
-      document.head.appendChild(script);
+            const script = document.createElement("script");
+            script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoApiKey}&autoload=false`;
+            script.async = true;
+            document.head.appendChild(script);
 
-      script.onload = () => {
-        window.kakao.maps.load(initMap);
-      };
+            script.onload = () => {
+                window.kakao.maps.load(initMap);
+            };
 
-      script.onerror = () => {
-        console.error("Kakao Maps API 스크립트를 로드하는데 실패했습니다.");
-      };
-    };
+            script.onerror = () => {
+                console.error(
+                    "Kakao Maps API 스크립트를 로드하는데 실패했습니다."
+                );
+            };
+        };
 
-    const initMap = () => {
-      const container = document.getElementById('map');
-      if (!container) {
-        console.error("지도를 표시할 HTML 요소를 찾을 수 없습니다.");
-        return;
-      }
-      const options = {
-        center: new window.kakao.maps.LatLng(37.32014600082093, 127.1288399333128),
-        level: 4,
-      };
-  const map = new window.kakao.maps.Map(container, options);
-  map.setZoomable(false); // 줌(축척) 고정
+        const initMap = () => {
+            const container = document.getElementById("map");
+            if (!container) {
+                console.error("지도를 표시할 HTML 요소를 찾을 수 없습니다.");
+                return;
+            }
+            const options = {
+                center: new window.kakao.maps.LatLng(
+                    37.32014600082093,
+                    127.1288399333128
+                ),
+                level: 4,
+            };
+            const map = new window.kakao.maps.Map(container, options);
+            map.setZoomable(false); // 줌(축척) 고정
 
-      // 단국대 평화의 광장
-      const busIconDiv1 = document.createElement('div');
-      busIconDiv1.style.width = '40px';
-      busIconDiv1.style.height = '40px';
-      busIconDiv1.style.display = 'flex';
-      busIconDiv1.style.alignItems = 'center';
-      busIconDiv1.style.justifyContent = 'center';
-      busIconDiv1.innerHTML = '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
+            // 단국대 평화의 광장
+            const busIconDiv1 = document.createElement("div");
+            busIconDiv1.style.width = "40px";
+            busIconDiv1.style.height = "40px";
+            busIconDiv1.style.display = "flex";
+            busIconDiv1.style.alignItems = "center";
+            busIconDiv1.style.justifyContent = "center";
+            busIconDiv1.innerHTML =
+                '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
 
-      const markerPosition1 = new window.kakao.maps.LatLng(37.32014600082093, 127.1288399333128);
-      const overlay1 = new window.kakao.maps.CustomOverlay({
-        position: markerPosition1,
-        content: busIconDiv1,
-        yAnchor: 1,
-      });
-      overlay1.setMap(map);
+            const markerPosition1 = new window.kakao.maps.LatLng(
+                37.32014600082093,
+                127.1288399333128
+            );
+            const overlay1 = new window.kakao.maps.CustomOverlay({
+                position: markerPosition1,
+                content: busIconDiv1,
+                yAnchor: 1,
+            });
+            overlay1.setMap(map);
 
-      // 단국대 종합 실험동
-      const busIconDiv2 = document.createElement('div');
-      busIconDiv2.style.width = '40px';
-      busIconDiv2.style.height = '40px';
-      busIconDiv2.style.display = 'flex';
-      busIconDiv2.style.alignItems = 'center';
-      busIconDiv2.style.justifyContent = 'center';
-      busIconDiv2.innerHTML = '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
+            // 단국대 종합 실험동
+            const busIconDiv2 = document.createElement("div");
+            busIconDiv2.style.width = "40px";
+            busIconDiv2.style.height = "40px";
+            busIconDiv2.style.display = "flex";
+            busIconDiv2.style.alignItems = "center";
+            busIconDiv2.style.justifyContent = "center";
+            busIconDiv2.innerHTML =
+                '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
 
-      const markerPosition2 = new window.kakao.maps.LatLng(37.32022368228002, 127.12572906480165);
-      const overlay2 = new window.kakao.maps.CustomOverlay({
-        position: markerPosition2,
-        content: busIconDiv2,
-        yAnchor: 1,
-      });
-      overlay2.setMap(map);
+            const markerPosition2 = new window.kakao.maps.LatLng(
+                37.32022368228002,
+                127.12572906480165
+            );
+            const overlay2 = new window.kakao.maps.CustomOverlay({
+                position: markerPosition2,
+                content: busIconDiv2,
+                yAnchor: 1,
+            });
+            overlay2.setMap(map);
 
-      // 단국대 치과병원
-      const busIconDiv3 = document.createElement('div');
-      busIconDiv3.style.width = '40px';
-      busIconDiv3.style.height = '40px';
-      busIconDiv3.style.display = 'flex';
-      busIconDiv3.style.alignItems = 'center';
-      busIconDiv3.style.justifyContent = 'center';
-      busIconDiv3.innerHTML = '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
+            // 단국대 치과병원
+            const busIconDiv3 = document.createElement("div");
+            busIconDiv3.style.width = "40px";
+            busIconDiv3.style.height = "40px";
+            busIconDiv3.style.display = "flex";
+            busIconDiv3.style.alignItems = "center";
+            busIconDiv3.style.justifyContent = "center";
+            busIconDiv3.innerHTML =
+                '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
 
-      const markerPosition3 = new window.kakao.maps.LatLng(37.322291863336666, 127.12543635052465);
-      const overlay3 = new window.kakao.maps.CustomOverlay({
-        position: markerPosition3,
-        content: busIconDiv3,
-        yAnchor: 1,
-      });
-      overlay3.setMap(map);
+            const markerPosition3 = new window.kakao.maps.LatLng(
+                37.322291863336666,
+                127.12543635052465
+            );
+            const overlay3 = new window.kakao.maps.CustomOverlay({
+                position: markerPosition3,
+                content: busIconDiv3,
+                yAnchor: 1,
+            });
+            overlay3.setMap(map);
 
-      // 죽전역
-      const busIconDiv4 = document.createElement('div');
-      busIconDiv4.style.width = '40px';
-      busIconDiv4.style.height = '40px';
-      busIconDiv4.style.display = 'flex';
-      busIconDiv4.style.alignItems = 'center';
-      busIconDiv4.style.justifyContent = 'center';
-      busIconDiv4.innerHTML = '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
+            // 죽전역
+            const busIconDiv4 = document.createElement("div");
+            busIconDiv4.style.width = "40px";
+            busIconDiv4.style.height = "40px";
+            busIconDiv4.style.display = "flex";
+            busIconDiv4.style.alignItems = "center";
+            busIconDiv4.style.justifyContent = "center";
+            busIconDiv4.innerHTML =
+                '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
 
-      const markerPosition4 = new window.kakao.maps.LatLng(37.32420554845601, 127.10820542281134);
-      const overlay4 = new window.kakao.maps.CustomOverlay({
-        position: markerPosition4,
-        content: busIconDiv4,
-        yAnchor: 1,
-      });
-      overlay4.setMap(map);
-    };
+            const markerPosition4 = new window.kakao.maps.LatLng(
+                37.32420554845601,
+                127.10820542281134
+            );
+            const overlay4 = new window.kakao.maps.CustomOverlay({
+                position: markerPosition4,
+                content: busIconDiv4,
+                yAnchor: 1,
+            });
+            overlay4.setMap(map);
 
-    loadKakaoMapScript();
+            // 단국대 정문 (5번째)
+            const busIconDiv5 = document.createElement("div");
+            busIconDiv5.style.width = "40px";
+            busIconDiv5.style.height = "40px";
+            busIconDiv5.style.display = "flex";
+            busIconDiv5.style.alignItems = "center";
+            busIconDiv5.style.justifyContent = "center";
+            busIconDiv5.innerHTML =
+                '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
 
-  }, []);
+            const markerPosition5 = new window.kakao.maps.LatLng(
+                37.323352264049944,
+                127.12596838722746
+            );
+            const overlay5 = new window.kakao.maps.CustomOverlay({
+                position: markerPosition5,
+                content: busIconDiv5,
+                yAnchor: 1,
+            });
+            overlay5.setMap(map);
 
-  return (
-    <div className="App">
-      <div id="map" style={{ width: '100vw', height: '100vh' }} />
-    </div>
-  );
+            // 단국대 상경관 (6번째)
+            const busIconDiv6 = document.createElement("div");
+            busIconDiv6.style.width = "40px";
+            busIconDiv6.style.height = "40px";
+            busIconDiv6.style.display = "flex";
+            busIconDiv6.style.alignItems = "center";
+            busIconDiv6.style.justifyContent = "center";
+            busIconDiv6.innerHTML =
+                '<img src="/ic_busstop.svg" alt="Bus Icon" width="40" height="40" />';
+
+            const markerPosition6 = new window.kakao.maps.LatLng(
+                37.32220999341863,
+                127.12826242041064
+            );
+            const overlay6 = new window.kakao.maps.CustomOverlay({
+                position: markerPosition6,
+                content: busIconDiv6,
+                yAnchor: 1,
+            });
+            overlay6.setMap(map);
+        };
+
+        loadKakaoMapScript();
+    }, []);
+
+    return (
+        <div className="App">
+            <div id="map" style={{ width: "100vw", height: "100vh" }} />
+        </div>
+    );
 }
 
 export default App;
--- src/KakaoMap.tsx
+++ src/KakaoMap.tsx
@@ -1,17 +1,17 @@
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const KakaoMap = () => {
-  const s_lat = 37.323105;
-  const s_lng = 126.97059787494679;
+    const s_lat = 37.323105;
+    const s_lng = 126.97059787494679;
 
-  return (
-    <Map
-      center={{ lat: s_lat, lng: s_lng }}
-      style={{ width: '100%', height: '360px' }}
-    >
-      <MapMarker position={{ lat: s_lat, lng: s_lng }}></MapMarker>
-    </Map>
-  );
+    return (
+        <Map
+            center={{ lat: s_lat, lng: s_lng }}
+            style={{ width: "100%", height: "360px" }}
+        >
+            <MapMarker position={{ lat: s_lat, lng: s_lng }}></MapMarker>
+        </Map>
+    );
 };
 
 export default KakaoMap;
