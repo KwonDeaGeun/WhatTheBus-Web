24f7cca7b5871cedb14b5ae3fc333d80b2a1ebf5
Kwon DaeGeun <dk31415@naver.com>
2025-09-11 10:26:06 +0900

    fix: 중복된 전역 선언 제거로 빌드 에러 픽스 및 메세지 핸들러 보강

--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -1,7 +1,13 @@
 export {};
 
 declare global {
-    interface Window {
-        kakao: any;
-    }
+  interface Window {
+    kakao: any;
+    map?: {
+      setCenter: (pos: unknown) => void;
+      setMinLevel: (level: number) => void;
+      setMaxLevel: (level: number) => void;
+      setZoomable: (zoomable: boolean) => void;
+    };
+  }
 }
