f2faa06ac9eef449484f046c24febbc3c0969cbf
Kwon DaeGeun <dk31415@naver.com>
2025-11-03 22:38:50 +0900

    fix: 테스트용 credential 제거

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import ky from "ky";
 const apiClient = ky.create({
     prefixUrl: import.meta.env.VITE_API_BASE_URL || "",
     timeout: 10000,
-    credentials: "include",
+    //credentials: "include",
     headers: {
         ...(import.meta.env.VITE_API_KEY ? { "x-api-key": import.meta.env.VITE_API_KEY } : {}),
     },
