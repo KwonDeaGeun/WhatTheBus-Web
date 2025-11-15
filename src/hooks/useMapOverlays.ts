b45ec65d9b4c4684797195ae46b616ca06af86f3
Kwon DaeGeun <dk31415@naver.com>
2025-11-15 14:07:31 +0900

    refactor: map 변경이 아닌 언마운트 시로 의존성 수정

--- src/hooks/useMapOverlays.ts
+++ src/hooks/useMapOverlays.ts
@@ -37,9 +37,6 @@ export const useMapOverlays = (
         };
     }, [map, busStops, selectedStopName, onStopClick]);
 
-    // map 변경 시 이전 캐시 정리 (cleanup에서만 실행)
-    // map이 변경될 때 이전 캐시를 정리하기 위해 의도적으로 의존성에 포함
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => {
         // 컴포넌트 언마운트 또는 map 변경 시 정리
         return () => {
